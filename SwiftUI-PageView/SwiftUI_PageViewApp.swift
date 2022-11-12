fc101b7e2af12be86bf5bae4527b4ba18cf22005
MaksBelenko <maksim.belenko@gmail.com>
2022-11-12 21:29:00 +0000

    Initial Commit

---
+++ SwiftUI-PageView/SwiftUI_PageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PageViewApp.swift
+//  SwiftUI-PageView
+//
+//  Created by Maksim on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
